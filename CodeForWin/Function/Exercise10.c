b6e9f125170b454fd723aeb7430957f3676f27e9
Ahmed-Alhaddad <ahmed91.alhaddad@gmail.com>
2023-02-25 14:46:34 +0200

    Modify

--- CodeForWin/Function/Exercise10.c
+++ CodeForWin/Function/Exercise10.c
@@ -1,7 +1,7 @@
 /*****************************************
  *
  *@file   :  
- *@Brief  :   
+ *@Brief  :   Write a C program to find power of any number using recursion.
  *@Auther :  Ahmed Waheed
  *****************************************
  */
@@ -11,24 +11,47 @@
 /*************** Include section end *****************************/
 
 /*************** Globel Variable section start *******************/
-
+static int Counter = 1;
 
 /*************** Globel Variable section end *********************/
 
 /*************** Decleration section start *********************/
-
+double Power(double Number, int base);
 /*************** Decleration section end *********************/
 
 int main()
 {
-
+	double Number = 0;
+	int base = 0;
+	printf("Please Enter The Power : ");
+	scanf("%d", &base);
+	printf("Please Enter The Number : ");
+	scanf("%lf", &Number);
+	printf("%.2lf ^ %d = %.2lf \n",Number, base, Power(Number, base));
+	
 
 
 	return 0;
 }
 
 /*************** Defenation Variable section start *********************/
-
+double Power(double Number, int base)
+{
+	
+	if(base == 0)
+	{
+		return 1;
+	}
+	else if(Number == 0)
+	{
+		return 0;
+	}
+	else
+	{
+		return Number * Power(Number, --base);
+		
+	}
+}
 
 
 
@@ -37,6 +60,6 @@ int main()
 /**************************************************
  *User                    Date                Brief
  **************************************************
- *Ahmed Waheed            
+ *Ahmed Waheed            				Write a C program to find power of any number using recursion.
  *****************************************************/
 
--- CodeForWin/Function/Exercise11.c
+++ CodeForWin/Function/Exercise11.c
@@ -1,7 +1,7 @@
 /*****************************************
  *
  *@file   :  
- *@Brief  :   
+ *@Brief  :   Write a C program to print all natural numbers between 1 to n using recursion.
  *@Auther :  Ahmed Waheed
  *****************************************
  */
@@ -16,19 +16,30 @@
 /*************** Globel Variable section end *********************/
 
 /*************** Decleration section start *********************/
-
+int Print(int Number);
 /*************** Decleration section end *********************/
 
 int main()
 {
-
-
+	int Number = 0;
+	printf("Please Enter the Number : ");
+	scanf("%d", &Number);
+	Print(Number);
+	
 
 	return 0;
 }
 
 /*************** Defenation Variable section start *********************/
-
+int Print(int Number)
+{
+	if(Number >= 1)
+	{
+		printf("%d \n",Number);
+	
+		Print(++Number);
+	}
+}
 
 
 
@@ -37,6 +48,6 @@ int main()
 /**************************************************
  *User                    Date                Brief
  **************************************************
- *Ahmed Waheed            
+ *Ahmed Waheed            				Write a C program to print all natural numbers between 1 to n using recursion.
  *****************************************************/
 
