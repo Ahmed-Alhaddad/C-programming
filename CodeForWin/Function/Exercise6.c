80b4a69fcd513dc6f7fad95e5670c60e1025e037
Ahmed-Alhaddad <ahmed91.alhaddad@gmail.com>
2023-02-28 23:50:57 +0200

    Modify

--- CodeForWin/Function/Exercise6.c
+++ CodeForWin/Function/Exercise6.c
@@ -1,7 +1,7 @@
 /*****************************************
  *
  *@file   :  
- *@Brief  :   
+ *@Brief  :   C program to find prime numbers in given range using functions
  *@Auther :  Ahmed Waheed
  *****************************************
  */
@@ -16,27 +16,67 @@
 /*************** Globel Variable section end *********************/
 
 /*************** Decleration section start *********************/
-
+int Prime(int Number);
+void Print(int NumberFirst, int NumberSecond);
 /*************** Decleration section end *********************/
 
 int main()
 {
-
-
+	int NumberFirst = 0;
+	int NumberSecond = 0;
+	printf("First Number : ");
+	scanf("%d", &NumberFirst);
+	printf("Second Number : ");
+	scanf("%d", &NumberSecond);
+	printf("Prime Numbers between %d and %d are : ", NumberFirst, NumberSecond);
+	Print(NumberFirst ,NumberSecond);
+	printf("\n");
 
 	return 0;
 }
 
 /*************** Defenation Variable section start *********************/
+void Print(int NumberFirst, int NumberSecond)
+{
+	while(NumberFirst <= NumberSecond)
+	{
+		if(0 == Prime(NumberFirst))
+		{
+			printf("%d, ", NumberFirst);
+		}
+		else
+		{
+			
+		}
+		NumberFirst++;
+		
+	}
 
-
-
+}
+int Prime(int Number)
+{
+	int counter = 0;
+	int flag = 0;
+	for(counter = 2; counter < Number; counter++)
+	{
+		if(Number % counter == 0)
+		{
+			flag = 1;
+			break;
+		}
+		else
+		{
+			flag = 0;
+		}
+	}
+	return flag;
+}
 
 /***************Defination Variable section end *********************/
 
 /**************************************************
  *User                    Date                Brief
  **************************************************
- *Ahmed Waheed            
+ *Ahmed Waheed            			C program to find prime numbers in given range using functions
  *****************************************************/
 
